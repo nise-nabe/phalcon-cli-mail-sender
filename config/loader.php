c213bb3b43ce765aa853379923f7337be7810699
nise_nabe <nise.nabe@gmail.com>
2014-12-08 18:52:08 +0900

    add comments

--- config/loader.php
+++ config/loader.php
@@ -4,10 +4,12 @@ $composerDir = APPLICATION_PATH.'/vendor/composer/';
 
 $loader = new \Phalcon\Loader();
 
+// composer
 $loader->registerClasses(
     require($composerDir.'autoload_classmap.php')
 );
 
+// tasks
 $loader->registerDirs([
     APPLICATION_PATH.'/tasks'
 ]);
