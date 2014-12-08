c3ff57f737339d530eb7e82977aee19a784f57c4
nise_nabe <nise.nabe@gmail.com>
2014-12-08 18:49:32 +0900

    add composer's libraries

--- config/loader.php
+++ config/loader.php
@@ -1,6 +1,12 @@
 <?php
 
+$composerDir = APPLICATION_PATH.'/vendor/composer/';
+
 $loader = new \Phalcon\Loader();
+
+$loader->registerClasses(
+    require($composerDir.'autoload_classmap.php')
+);
 
 $loader->registerDirs([
     APPLICATION_PATH.'/tasks'
