3b4586cce86a7b425ab2abc0627f110db72484ba
nise_nabe <nise.nabe@gmail.com>
2014-12-08 19:20:03 +0900

    add db settings

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'  => 'Mysql',
+        'host'     => '',
+        'username' => '',
+        'password' => '',
+        'dbname'   => '',
+        'charset'  => 'utf8mb4',
+    ),
+));
--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,11 +1,23 @@
 <?php
 
 use Phalcon\CLI\Console as ConsoleApp;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 
 class Bootstrap extends ConsoleApp
 {
     public function __construct($di)
     {
         parent::__construct($di);
+
+        $di->set('db', function () use ($di) {
+            $config = $di->get('config');
+            $db = new DbAdapter(array(
+                'host'     => $config->database->host,
+                'username' => $config->database->username,
+                'password' => $config->database->password,
+                'dbname'   => $config->database->dbname
+            ));
+            return $db;
+        });
     }
 }
